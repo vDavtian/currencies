c580b2edcd79dc400e8a9310bcea61e3b583eece
vDavtian <davtyan.v.d@gmail.com>
2021-10-24 15:59:06 +0400

    Table styles

--- src/components/CurrencyTable.js
+++ src/components/CurrencyTable.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import {
-    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper
+    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, makeStyles
 } from '@material-ui/core';
 import { Delete, Edit } from '@material-ui/icons';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        background: '#1f233f',
+        // margin: '10px'
+    },
+    table: {
+        minWidth: 700
+    },
+    tableCell: {
+        color: '#dfe1f1',
+    },
+}));
+
 const CurrencyTable = ({ data, toggleDialog, setSelectedRow }) => {
+    const classes = useStyles();
+
     return (
-        <TableContainer style={{ border: '1px solid pink', margin: '10px' }} component={Paper}>
-            <Table>
-                <TableHead style={{ borderBottom: '1px solid #e0e0e0' }}>
+        <TableContainer className={classes.root} component={Paper} >
+            <Table className={classes.table}>
+                <TableHead
+                    style={{ borderBottom: '1px solid #e0e0e0' }}
+                >
                     <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Currency Name</TableCell>
-                        <TableCell>Rate (1$ = X rate)</TableCell>
+                        <TableCell className={classes.tableCell}>ID</TableCell>
+                        <TableCell className={classes.tableCell}>Currency Name</TableCell>
+                        <TableCell className={classes.tableCell}>Rate (1$ = X rate)</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {data.map((item) => (
                         <TableRow key={item.id}>
-                            <TableCell>{item.currencyId}</TableCell>
-                            <TableCell>{item.name}</TableCell>
-                            <TableCell>{item.rate}</TableCell>
-                            <TableCell align="right">
+                            <TableCell className={classes.tableCell}>{item.currencyId}</TableCell>
+                            <TableCell className={classes.tableCell}>{item.name}</TableCell>
+                            <TableCell className={classes.tableCell}>{item.rate}</TableCell>
+                            <TableCell className={classes.tableCell} align="right">
                                 <Edit
                                     onClick={() => {
                                         toggleDialog();
